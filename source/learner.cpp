bf2c4aadad1f16dda0ea4b4fd7027072db78b70d
Hisayori Noda <nodchip@gmail.com>
2016-06-01 09:17:22 +0900

    学習ルーチンをマルチスレッド化した

--- source/learner.cpp
+++ source/learner.cpp
@@ -44,7 +44,7 @@ namespace
   constexpr VALUE_TYPE EPS = 1e-8;
   constexpr VALUE_TYPE ADAM_BETA1 = 0.9;
   constexpr VALUE_TYPE ADAM_BETA2 = 0.999;
-  constexpr VALUE_TYPE LEARNING_RATE = 0.001;
+  constexpr VALUE_TYPE LEARNING_RATE = 0.01;
   constexpr int MAX_GAME_PLAY = 256;
 
   std::ifstream kifu_file_stream;
@@ -60,6 +60,9 @@ namespace
 
   bool read_position_and_value(Position& pos, Value& value)
   {
+    static std::mutex mutex;
+    std::lock_guard<std::mutex> lock_guard(mutex);
+
     if (static_cast<int>(position_and_values.size()) <= position_and_value_index) {
       position_and_values.clear();
       position_and_value_index = 0;
@@ -195,131 +198,160 @@ void Learner::learn()
     }
   }
 
-  Thread& thread = *Threads.main();
-
   Search::LimitsType limits;
   limits.max_game_ply = MAX_GAME_PLAY;
   limits.depth = 1;
   limits.silent = true;
   Search::Limits = limits;
 
-  Position& position = thread.rootPos;
-  Value recordValue;
-  int64_t current_index = 0;
-  while (read_position_and_value(position, recordValue)) {
-    // 浅い探索を行う
-    // 本当はqsearch()で行いたいが、直接呼んだらクラッシュしたので…。
-    thread.rootMoves.clear();
-    // 王手が掛かっているかどうかに応じてrootMovesの種類を変える
-    if (position.in_check()) {
-      for (auto m : MoveList<EVASIONS>(position)) {
-        thread.rootMoves.push_back(Search::RootMove(m));
+  std::atomic_int64_t global_current_index = 0;
+  std::vector<std::thread> threads;
+  while (threads.size() < Threads.size()) {
+    int thread_index = static_cast<int>(threads.size());
+    auto procedure = [&global_current_index, &threads, &w, &m, &v, &sum_w, &last_update_indexes, thread_index] {
+      Thread& thread = *Threads[thread_index];
+
+      Position& pos = thread.rootPos;
+      pos.set_this_thread(&thread);
+      Value recordValue;
+      while (read_position_and_value(pos, recordValue)) {
+        pos.check_info_update();
+
+        int64_t current_index = global_current_index++;
+        // 浅い探索を行う
+        // 本当はqsearch()で行いたいが、直接呼んだらクラッシュしたので…。
+        thread.rootMoves.clear();
+        // 王手が掛かっているかどうかに応じてrootMovesの種類を変える
+        if (pos.in_check()) {
+          for (auto m : MoveList<EVASIONS>(pos)) {
+            if (pos.legal(m)) {
+              thread.rootMoves.push_back(Search::RootMove(m));
+            }
+          }
+        }
+        else {
+          for (auto m : MoveList<CAPTURES_PRO_PLUS>(pos)) {
+            if (pos.legal(m)) {
+              thread.rootMoves.push_back(Search::RootMove(m));
+            }
+          }
+          for (auto m : MoveList<QUIET_CHECKS>(pos)) {
+            if (pos.legal(m)) {
+              thread.rootMoves.push_back(Search::RootMove(m));
+            }
+          }
+        }
+        //for (auto m : MoveList<LEGAL>(pos)) {
+        //  thread.rootMoves.push_back(Search::RootMove(m));
+        //}
+
+        Color rootColor = pos.side_to_move();
+
+        Value value;
+        if (thread.rootMoves.empty()) {
+          // 現在の局面が静止している状態なのだと思う
+          // 現在の局面の評価値をそのまま使う
+          value = Eval::compute_eval(pos);
+        }
+        else {
+          // 実際に探索する
+          thread.maxPly = 0;
+          thread.rootDepth = 0;
+          pos.set_this_thread(&thread);
+
+          //std::cerr << pos << std::endl;
+
+          thread.search();
+          //Value debugValue = thread.rootMoves[0].score;
+          value = thread.rootMoves[0].score;
+
+          // 静止した局面まで進める
+          StateInfo stateInfo[MAX_PLY];
+          int play = 0;
+          for (auto m : thread.rootMoves[0].pv) {
+            pos.do_move(m, stateInfo[play++]);
+            //Eval::evaluate(pos);
+          }
+          //value = Eval::evaluate(pos);
+          //if (rootColor != pos.side_to_move()) {
+          //  value = -value;
+          //}
+          //std::cerr << pos << std::endl;
+          //std::cerr << "debugValue=" << debugValue << " value=" << value << std::endl;
+          //ASSERT_LV3(value == debugValue);
+        }
+
+        // 深い深さの探索による評価値との差分を求める
+        VALUE_TYPE delta = static_cast<VALUE_TYPE>((recordValue - value) * FV_SCALE);
+        // Aperyだと[0]に相当する値は現在の局面の手番で場合分けしている
+        VALUE_TYPE delta0 = rootColor == BLACK ? delta : -delta;
+        // Aperyだと[1]に相当する値は現在の局面の手番と
+        // 浅い探索のPVの末端の局面の手番が等しいかどうかで場合分けしている
+        // TODO(tanuki-): 理由を考える
+        VALUE_TYPE delta1 = rootColor == pos.side_to_move() ? delta : -delta;
+
+        // 値を更新する
+        Square sq_bk = pos.king_square(BLACK);
+        Square sq_wk = pos.king_square(WHITE);
+        const auto& list0 = pos.eval_list()->piece_list_fb();
+        const auto& list1 = pos.eval_list()->piece_list_fw();
+
+        // KK
+        increase_parameters(current_index, delta0, kk_index_to_raw_index(sq_bk, sq_wk, BLACK), Eval::kk[sq_bk][sq_wk][BLACK], w, m, v, sum_w, last_update_indexes);
+        increase_parameters(current_index, delta1, kk_index_to_raw_index(sq_bk, sq_wk, WHITE), Eval::kk[sq_bk][sq_wk][WHITE], w, m, v, sum_w, last_update_indexes);
+
+        for (int i = 0; i < PIECE_NO_KING; ++i) {
+          Eval::BonaPiece k0 = list0[i];
+          Eval::BonaPiece k1 = list1[i];
+          for (int j = 0; j < i; ++j) {
+            Eval::BonaPiece l0 = list0[j];
+            Eval::BonaPiece l1 = list1[j];
+
+            // KPP
+            increase_parameters(current_index, delta0, kpp_index_to_raw_index(sq_bk, k0, l0, BLACK), Eval::kpp[sq_bk][k0][l0][BLACK], w, m, v, sum_w, last_update_indexes);
+            increase_parameters(current_index, delta1, kpp_index_to_raw_index(sq_bk, k0, l0, WHITE), Eval::kpp[sq_bk][k0][l0][WHITE], w, m, v, sum_w, last_update_indexes);
+
+            // KPP
+            increase_parameters(current_index, -delta0, kpp_index_to_raw_index(Inv(sq_wk), k1, l1, BLACK), Eval::kpp[Inv(sq_wk)][k1][l1][BLACK], w, m, v, sum_w, last_update_indexes);
+            increase_parameters(current_index, delta1, kpp_index_to_raw_index(Inv(sq_wk), k1, l1, WHITE), Eval::kpp[Inv(sq_wk)][k1][l1][WHITE], w, m, v, sum_w, last_update_indexes);
+          }
+
+          // KKP
+          increase_parameters(current_index, delta0, kkp_index_to_raw_index(sq_bk, sq_wk, k0, BLACK), Eval::kkp[sq_bk][sq_wk][k0][BLACK], w, m, v, sum_w, last_update_indexes);
+          increase_parameters(current_index, delta1, kkp_index_to_raw_index(sq_bk, sq_wk, k0, WHITE), Eval::kkp[sq_bk][sq_wk][k0][WHITE], w, m, v, sum_w, last_update_indexes);
+        }
+
+        if (current_index % 10000 == 0) {
+          Value value_after = Eval::compute_eval(pos);
+          if (rootColor != pos.side_to_move()) {
+            value_after = -value_after;
+          }
+
+          fprintf(stderr, "index=%I64d recorded=%5d before=%5d after=%5d delta=%5d target=%c turn=%s\n",
+            current_index,
+            static_cast<int>(recordValue),
+            static_cast<int>(value),
+            static_cast<int>(value_after),
+            static_cast<int>(value_after - value),
+            delta > 0 ? '+' : '-',
+            rootColor == BLACK ? "black" : "white");
+        }
+
+        // 局面は元に戻さなくても大丈夫
       }
-    }
-    else {
-      for (auto m : MoveList<CAPTURES_PRO_PLUS>(position)) {
-        thread.rootMoves.push_back(Search::RootMove(m));
-      }
-      for (auto m : MoveList<QUIET_CHECKS>(position)) {
-        thread.rootMoves.push_back(Search::RootMove(m));
-      }
-    }
-    //for (auto m : MoveList<LEGAL>(position)) {
-    //  thread.rootMoves.push_back(Search::RootMove(m));
-    //}
+    };
 
-    Value value;
-    if (thread.rootMoves.empty()) {
-      // 現在の局面が静止している状態なのだと思う
-      // 現在の局面の評価値をそのまま使う
-      value = Eval::compute_eval(position);
-    }
-    else {
-      // 実際に探索する
-      thread.maxPly = 0;
-      thread.rootDepth = 0;
-      position.set_this_thread(&thread);
-
-      //std::cerr << pos << std::endl;
-
-      thread.Thread::start_searching();
-      thread.wait_for_search_finished();
-      value = thread.rootMoves[0].score;
-    }
-
-    Color rootColor = position.side_to_move();
-
-    // 静止した局面まで進める
-    StateInfo stateInfo[MAX_PLY];
-    int play = 0;
-    for (auto m : thread.rootMoves[0].pv) {
-      position.do_move(m, stateInfo[play++]);
-    }
-
-    // 深い深さの探索による評価値との差分を求める
-    VALUE_TYPE delta = static_cast<VALUE_TYPE>((recordValue - value) * FV_SCALE);
-    // Aperyだと[0]に相当する値は現在の局面の手番で場合分けしている
-    VALUE_TYPE delta0 = rootColor == BLACK ? delta : -delta;
-    // Aperyだと[1]に相当する値は現在の局面の手番と
-    // 浅い探索のPVの末端の局面の手番が等しいかどうかで場合分けしている
-    // TODO(tanuki-): 理由を考える
-    VALUE_TYPE delta1 = rootColor == position.side_to_move() ? delta : -delta;
-
-    // 値を更新する
-    Square sq_bk = position.king_square(BLACK);
-    Square sq_wk = position.king_square(WHITE);
-    const auto& list0 = position.eval_list()->piece_list_fb();
-    const auto& list1 = position.eval_list()->piece_list_fw();
-
-    // KK
-    increase_parameters(current_index, delta0, kk_index_to_raw_index(sq_bk, sq_wk, BLACK), Eval::kk[sq_bk][sq_wk][BLACK], w, m, v, sum_w, last_update_indexes);
-    increase_parameters(current_index, delta1, kk_index_to_raw_index(sq_bk, sq_wk, WHITE), Eval::kk[sq_bk][sq_wk][WHITE], w, m, v, sum_w, last_update_indexes);
-
-    for (int i = 0; i < PIECE_NO_KING; ++i) {
-      Eval::BonaPiece k0 = list0[i];
-      Eval::BonaPiece k1 = list1[i];
-      for (int j = 0; j < i; ++j) {
-        Eval::BonaPiece l0 = list0[j];
-        Eval::BonaPiece l1 = list1[j];
-
-        // KPP
-        increase_parameters(current_index, delta0, kpp_index_to_raw_index(sq_bk, k0, l0, BLACK), Eval::kpp[sq_bk][k0][l0][BLACK], w, m, v, sum_w, last_update_indexes);
-        increase_parameters(current_index, delta1, kpp_index_to_raw_index(sq_bk, k0, l0, WHITE), Eval::kpp[sq_bk][k0][l0][WHITE], w, m, v, sum_w, last_update_indexes);
-
-        // KPP
-        increase_parameters(current_index, -delta0, kpp_index_to_raw_index(Inv(sq_wk), k1, l1, BLACK), Eval::kpp[Inv(sq_wk)][k1][l1][BLACK], w, m, v, sum_w, last_update_indexes);
-        increase_parameters(current_index, delta1, kpp_index_to_raw_index(Inv(sq_wk), k1, l1, WHITE), Eval::kpp[Inv(sq_wk)][k1][l1][WHITE], w, m, v, sum_w, last_update_indexes);
-      }
-
-      // KKP
-      increase_parameters(current_index, delta0, kkp_index_to_raw_index(sq_bk, sq_wk, k0, BLACK), Eval::kkp[sq_bk][sq_wk][k0][BLACK], w, m, v, sum_w, last_update_indexes);
-      increase_parameters(current_index, delta1, kkp_index_to_raw_index(sq_bk, sq_wk, k0, WHITE), Eval::kkp[sq_bk][sq_wk][k0][WHITE], w, m, v, sum_w, last_update_indexes);
-    }
-
-    if (current_index % 1000 == 0) {
-      Value value_after = Eval::compute_eval(position);
-      if (rootColor != position.side_to_move()) {
-        value_after = -value_after;
-      }
-
-      fprintf(stderr, "recorded=%5d before=%5d after=%5d delta=%5d target=%c turn=%s\n",
-        static_cast<int>(recordValue),
-        static_cast<int>(value),
-        static_cast<int>(value_after),
-        static_cast<int>(value_after - value),
-        delta > 0 ? '+' : '-',
-        rootColor == BLACK ? "black" : "white");
-    }
-
-    // 局面は元に戻さなくても大丈夫
-    ++current_index;
+    threads.emplace_back(procedure);
+  }
+  for (auto& thread : threads) {
+    thread.join();
   }
 
   for (Square k : SQ) {
     for (Eval::BonaPiece p0 = Eval::BONA_PIECE_ZERO; p0 < Eval::fe_end; ++p0) {
       for (Eval::BonaPiece p1 = Eval::BONA_PIECE_ZERO; p1 < Eval::fe_end; ++p1) {
         for (Color c = COLOR_ZERO; c < COLOR_NB; ++c) {
-          clamp_and_store(current_index, kpp_index_to_raw_index(k, p0, p1, c), Eval::kpp[k][p0][p1][c], sum_w, w, last_update_indexes);
+          clamp_and_store(global_current_index, kpp_index_to_raw_index(k, p0, p1, c), Eval::kpp[k][p0][p1][c], sum_w, w, last_update_indexes);
         }
       }
     }
@@ -328,7 +360,7 @@ void Learner::learn()
     for (Square k1 : SQ) {
       for (Eval::BonaPiece p = Eval::BONA_PIECE_ZERO; p < Eval::fe_end; ++p) {
         for (Color c = COLOR_ZERO; c < COLOR_NB; ++c) {
-          clamp_and_store(current_index, kkp_index_to_raw_index(k0, k1, p, c), Eval::kkp[k0][k1][p][c], sum_w, w, last_update_indexes);
+          clamp_and_store(global_current_index, kkp_index_to_raw_index(k0, k1, p, c), Eval::kkp[k0][k1][p][c], sum_w, w, last_update_indexes);
         }
       }
     }
@@ -336,7 +368,7 @@ void Learner::learn()
   for (Square k0 : SQ) {
     for (Square k1 : SQ) {
       for (Color c = COLOR_ZERO; c < COLOR_NB; ++c) {
-        clamp_and_store(current_index, kk_index_to_raw_index(k0, k1, c), Eval::kk[k0][k1][c], sum_w, w, last_update_indexes);
+        clamp_and_store(global_current_index, kk_index_to_raw_index(k0, k1, c), Eval::kk[k0][k1][c], sum_w, w, last_update_indexes);
       }
     }
   }
