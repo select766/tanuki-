9b51246d90f550040d44022b18de4ff1e8903416
Hisayori Noda <nodchip@gmail.com>
2016-10-25 18:51:27 +0900

    生の出現頻度を直接出力するようにした

--- source/experimental_learner.cpp
+++ source/experimental_learner.cpp
@@ -914,24 +914,24 @@ void Learner::CalculateAppearanceFrequencyHistogram() {
   }
 
   // èoåªïpìx, ì¡í•ó àˆéqÇÃêî
-  sync_cout << "Calculating appearance frequencies." << sync_endl;
-  std::map<int, int> result;
-  constexpr int cutoff = 1;
-  for (int appearance_frequency : appearance_frequencies) {
-    ++result[appearance_frequency / cutoff * cutoff];
-  }
+  //sync_cout << "Calculating appearance frequencies." << sync_endl;
+  //std::map<int, int> result;
+  //constexpr int cutoff = 1;
+  //for (int appearance_frequency : appearance_frequencies) {
+  //  ++result[appearance_frequency / cutoff * cutoff];
+  //}
 
-  int last = result.rbegin()->first;
+  //int last = result.rbegin()->first;
 
   std::string output_file_path = Options[Learner::OPTION_APPEARANCE_FREQUENCY_HISTOGRAM_OUTPUT_FILE_PATH];
   sync_cout << "Writing the result to " << output_file_path << sync_endl;
-  std::ofstream ofs(output_file_path);
-  //for (int i = 0; i <= last; i += cutoff) {
-  //  ofs << i << "," << result[i] << std::endl;
-  //}
-  for (const auto& p : result) {
-    ofs << p.first << "," << p.second << std::endl;
+  FILE* file = std::fopen(output_file_path.c_str(), "wt");
+  std::setvbuf(file, nullptr, _IOFBF, 1024 * 1024);
+  for (int appearance_frequency : appearance_frequencies) {
+    fprintf(file, "%d\n", appearance_frequency);
   }
+  std::fclose(file);
+  file = nullptr;
 
   auto elapsed = std::chrono::system_clock::now() - start;
   double elapsed_sec = static_cast<double>(
