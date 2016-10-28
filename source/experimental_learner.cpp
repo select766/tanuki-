cd60cf4969169ea742090a5ea472f81b4683be78
Hisayori Noda <nodchip@gmail.com>
2016-10-28 18:48:41 +0900

    current date timeに終了予定時刻が表示されてしまうバグを修正した

--- source/experimental_learner.cpp
+++ source/experimental_learner.cpp
@@ -269,8 +269,8 @@ void Learner::ShowProgress(const time_t& start_time, int64_t current_data, int64
   int second = elapsed_time % 60;
   double data_per_time = current_data / static_cast<double>(current_time - start_time);
   time_t expected_time = current_time + (total_data - current_data) / data_per_time;
-  struct tm *current_tm = std::localtime(&current_time);
-  struct tm *expected_tm = std::localtime(&expected_time);
+  struct tm current_tm = *std::localtime(&current_time);
+  struct tm expected_tm = *std::localtime(&expected_time);
   std::printf(
       "%lld / %lld\n"
       "        elapsed time = %02d:%02d:%02d\n"
@@ -278,8 +278,8 @@ void Learner::ShowProgress(const time_t& start_time, int64_t current_data, int64
       "    finish date time = %04d-%02d-%02d %02d:%02d:%02d\n",
     current_data, total_data,
     hour, minute, second,
-    current_tm->tm_year + 1900, current_tm->tm_mon + 1, current_tm->tm_mday, current_tm->tm_hour, current_tm->tm_min, current_tm->tm_sec,
-    expected_tm->tm_year + 1900, expected_tm->tm_mon + 1, expected_tm->tm_mday, expected_tm->tm_hour, expected_tm->tm_min, expected_tm->tm_sec);
+    current_tm.tm_year + 1900, current_tm.tm_mon + 1, current_tm.tm_mday, current_tm.tm_hour, current_tm.tm_min, current_tm.tm_sec,
+    expected_tm.tm_year + 1900, expected_tm.tm_mon + 1, expected_tm.tm_mday, expected_tm.tm_hour, expected_tm.tm_min, expected_tm.tm_sec);
   std::fflush(stdout);
 }
 
