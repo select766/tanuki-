2764636482b4465b7c49f0a5b055fa6a0fba74ec
Hisayori Noda <nodchip@gmail.com>
2017-01-03 16:11:14 +0900

    出力するデータの指定の誤りを修正した

--- tools/zoukin.py
+++ tools/zoukin.py
@@ -134,7 +134,7 @@ go btime {num_games} wtime 24 byoyomi 1
     winning_percentage = 0.0
     if lose + draw + win > 0.0:
       winning_percentage = win / (lose + draw + win)
-    csvwriter.writerow([new_eval_folder_path, subfolder, winning_percentage])
+    csvwriter.writerow([new_eval_folder_path, winning_percentage])
 
 
 def GetSubfolders(folder_path):
