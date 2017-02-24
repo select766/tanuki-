1a9a186401a77ad968b4fd76866a95ea31b69fa1
Hisayori Noda <nodchip@gmail.com>
2017-02-24 23:52:31 +0900

    進捗表示ライブラリを追加した

---
+++ source/progress_report.cpp
@@ -0,0 +1,56 @@
+#include "progress_report.h"
+
+#include <cstdio>
+#include <ctime>
+
+namespace {
+  double kAlpha = 0.1;
+}
+
+ProgressReport::ProgressReport(int64_t total_data, int64_t show_at_most_sec)
+  : total_data_(total_data), show_at_most_sec_(show_at_most_sec), previous_data_(0),
+  previous_time_(std::time(nullptr)), start_time_(std::time(nullptr)) {
+}
+
+void ProgressReport::Show(int64_t current_data) {
+  if (current_data == 0) {
+    return;
+  }
+
+  time_t current_time = std::time(nullptr);
+  if (previous_time_ + show_at_most_sec_ > current_time) {
+    return;
+  }
+
+  time_t elapsed_time = current_time - start_time_;
+  int hour = static_cast<int>(elapsed_time / 3600);
+  int minute = elapsed_time / 60 % 60;
+  int second = elapsed_time % 60;
+
+  time_t duration = current_time - previous_time_;
+  exponential_moving_averaged_time =
+    kAlpha * duration + (1.0 - kAlpha) * exponential_moving_averaged_time;
+
+  int64_t diff = current_data - previous_data_;
+  exponential_moving_averaged_data =
+    kAlpha * diff + (1.0 - kAlpha) * exponential_moving_averaged_data;
+
+  double time_per_data = exponential_moving_averaged_time / exponential_moving_averaged_data;
+  time_t expected_time =
+    static_cast<time_t>(current_time + (total_data_ - current_data) * time_per_data);
+  struct tm current_tm = *std::localtime(&current_time);
+  struct tm expected_tm = *std::localtime(&expected_time);
+  std::printf(
+    "%lld / %lld\n"
+    "        elapsed time = %02d:%02d:%02d\n"
+    "   current date time = %04d-%02d-%02d %02d:%02d:%02d\n"
+    "    finish date time = %04d-%02d-%02d %02d:%02d:%02d\n",
+    current_data, total_data_,
+    hour, minute, second,
+    current_tm.tm_year + 1900, current_tm.tm_mon + 1, current_tm.tm_mday, current_tm.tm_hour, current_tm.tm_min, current_tm.tm_sec,
+    expected_tm.tm_year + 1900, expected_tm.tm_mon + 1, expected_tm.tm_mday, expected_tm.tm_hour, expected_tm.tm_min, expected_tm.tm_sec);
+  std::fflush(stdout);
+
+  previous_data_ = current_data;
+  previous_time_ = current_time;
+}
