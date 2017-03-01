df06cbf00ffb05942287df1ca14d89d64037af58
Hisayori Noda <nodchip@gmail.com>
2017-03-01 20:22:15 +0900

    固定するパラメーターを除外した

--- tanuki-optimizer/parameters_generator.py
+++ tanuki-optimizer/parameters_generator.py
@@ -8,6 +8,10 @@ def main():
     reader = csv.reader(f)
     for index, row in enumerate(reader):
       name, default_value, min_value, max_value = row
+      #if not name.endswith('_OPENING'):
+      #  continue
+      if not name.endswith('_ENDING'):
+        continue
       #print("{0}={1}".format(name, default_value))
       #print('-D{0}=$({0})'.format(name))
       #print("  hp.quniform('{0}', {1}, {2}, 1),".format(name, min_value, max_value))
@@ -17,16 +21,15 @@ def main():
       #print('#endif')
       #print("{0}={1}".format(name, str(int(parameters[index]))))
       #print("  '{0}',".format(name))
-      if name.endswith('_OPENING'):
-        sigma = (float(max_value) - float(min_value)) / 6.0
-      else:
-        sigma = 1e-8
+      #sigma = (float(max_value) - float(min_value)) / 6.0
       #print("  hp.qnormal('{0}', {1}, {2}, 1),".format(name, default_value, sigma))
+      #print("  '{0}',".format(name))
       #print('  {name},'.format(name=name))
-      print("    f.write('PARAM_DEFINE {name} = {{{name}}};\\n'.format({name}=int(round(scipy.stats.norm.ppf({name}, loc={default_value}, scale={sigma})))))".format(name=name, default_value=default_value, sigma=sigma))
+      #print("    f.write('PARAM_DEFINE {name} = {{{name}}};\\n'.format({name}=int(round(scipy.stats.norm.ppf({name}, loc={default_value}, scale={sigma})))))".format(name=name, default_value=default_value, sigma=sigma))
       #print("  STATE['{name}'].append({name})".format(name=name))
       #print("  '{name}': [0.0, 1.0],".format(name=name))
       #print("  print('{name:50s}= {{{name}}}'.format({name}={name}))".format(name=name))
+      print('PARAM_DEFINE {name} = {default_value};'.format(name=name, default_value=default_value))
 
 
 if __name__ == '__main__':
