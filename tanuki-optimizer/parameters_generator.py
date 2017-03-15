34ce875f11219d786fbe2029cff5e003d06080ef
Hisayori Noda <nodchip@gmail.com>
2017-03-15 14:50:07 +0900

    パラメーターの選択の際の分布を一様分布にした

--- tanuki-optimizer/parameters_generator.py
+++ tanuki-optimizer/parameters_generator.py
@@ -2,12 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import csv
+import math
 
 def main():
   with open('param/parameters.opening.csv', 'r') as f:
     reader = csv.reader(f)
     for index, row in enumerate(reader):
       name, default_value, min_value, max_value = row
+      default_value = int(default_value)
+      min_value = int(min_value)
+      max_value = int(max_value)
       #if not name.endswith('_OPENING'):
       #  continue
       #if not name.endswith('_ENDING'):
@@ -21,8 +25,8 @@ def main():
       #print('#endif')
       #print("{0}={1}".format(name, str(int(parameters[index]))))
       #print("  '{0}',".format(name))
-      sigma = float(default_value) / 10.0 / 3.0
-      print("  hp.qnormal('{0}', {1}, {2}, 1),".format(name, default_value, sigma))
+      #sigma = float(default_value) / 10.0 / 3.0
+      #print("  hp.qnormal('{0}', {1}, {2}, 1),".format(name, default_value, sigma))
       #print("  '{0}',".format(name))
       #print('  {name},'.format(name=name))
       #print("    f.write('PARAM_DEFINE {name} = {{{name}}};\\n'.format({name}=int(round(scipy.stats.norm.ppf({name}, loc={default_value}, scale={sigma})))))".format(name=name, default_value=default_value, sigma=sigma))
@@ -30,6 +34,9 @@ def main():
       #print("  '{name}': [0.0, 1.0],".format(name=name))
       #print("  print('{name:50s}= {{{name}}}'.format({name}={name}))".format(name=name))
       #print('PARAM_DEFINE {name} = {default_value};'.format(name=name, default_value=default_value))
+      min_value = max(min_value, int(math.floor(float(default_value) * 0.75)))
+      max_value = min(max_value, int(math.ceil(float(default_value) * 1.25)))
+      print("  hp.quniform('{0}', {1}, {2}, 1),".format(name, min_value, max_value))
 
 
 if __name__ == '__main__':
