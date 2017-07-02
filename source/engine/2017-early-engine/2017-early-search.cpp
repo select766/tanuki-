7ce39ff7bbc1ef69f72dbba50392799b6f214a62
yaneruao <yaneurao@gmail.com>
2017-07-02 13:07:23 +0900

    Learner::search()をスレッドごとのmulti pvに対応させた。
    # Conflicts:
    #	source/engine/2017-early-engine/2017-early-search.cpp
    #	source/learn/learn.h
    #	source/search.h

--- source/engine/2017-early-engine/2017-early-search.cpp
+++ source/engine/2017-early-engine/2017-early-search.cpp
@@ -2924,14 +2924,17 @@ namespace Learner
 	//   auto v = search(pos,3);
 	// のようにすべし。
 	// v.firstに評価値、v.secondにPVが得られる。
-	// MultiPVが有効のときは、pos.this_thread()->rootMoves[N].pvにそのPV(読み筋)の配列が得られる。
+	// multi pvが有効のときは、pos.this_thread()->rootMoves[N].pvにそのPV(読み筋)の配列が得られる。
+	// multi pvの指定はこの関数の引数multiPVで行なう。(Options["MultiPV"]の値は無視される)
+	// 
+	// 返されるpv配列には宣言勝ち(MOVE_WIN)も含まれるので注意。
 	//
 	// 前提条件) pos.set_this_thread(Threads[thread_id])で探索スレッドが設定されていること。
 	// 　また、Signals.stopが来ると探索を中断してしまうので、そのときのPVは正しくない。
 	// 　search()から戻ったあと、Signals.stop == trueなら、その探索結果を用いてはならない。
 	// 　あと、呼び出し前は、Signals.stop == falseの状態で呼び出さないと、探索を中断して返ってしまうので注意。
 
-	pair<Value, vector<Move> > search(Position& pos, int depth_)
+	pair<Value, vector<Move> > search(Position& pos, int depth_, size_t multiPV /* = 1*/)
 	{
 		Depth depth = depth_ * ONE_PLY;
 		if (depth < DEPTH_ZERO)
@@ -2955,7 +2958,8 @@ namespace Learner
 		auto& completedDepth = th->completedDepth;
 
 		// bestmoveとしてしこの局面の上位N個を探索する機能
-		size_t multiPV = Options["MultiPV"];
+		//size_t multiPV = Options["MultiPV"];
+
 		// この局面での指し手の数を上回ってはいけない
 		multiPV = std::min(multiPV, rootMoves.size());
 
