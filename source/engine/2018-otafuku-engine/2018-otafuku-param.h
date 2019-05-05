f277aa5bc1a49c30ab7eea2a821d0e3440479427
Hisayori Noda <nodchip@gmail.com>
2019-05-05 12:33:47 +0900

    探索パラメーターをもとに戻した

--- source/engine/2018-otafuku-engine/2018-otafuku-param.h
+++ source/engine/2018-otafuku-engine/2018-otafuku-param.h
@@ -1,125 +1,268 @@
-﻿#ifndef __PARAMETERS_GENERATED_HPP__
-#define __PARAMETERS_GENERATED_HPP__
-// Created at: 2019-04-15 11:02:11
-// Log: optimize_parameters.hyperopt_state.20190313_173749.pickle
-// Parameters CSV: param/2018-otafuku-param.original.h
-// minima_method = Data
-// n_iterations_to_use = 1000
+﻿#ifndef _2018_OTAFUKU_PARAMETERS_
+#define _2018_OTAFUKU_PARAMETERS_
 
-// |--------@---------| raw=169.999999995, min=100, max=240 default=172
-PARAM_DEFINE PARAM_FUTILITY_MARGIN_ALPHA1 = 170;
+// パラメーターの説明に "fixed"と書いてあるパラメーターはランダムパラメーター化するときでも変化しない。
+// 「前提depth」は、これ以上ならその枝刈りを適用する(かも)の意味。
+// 「適用depth」は、これ以下ならその枝刈りを適用する(かも)の意味。
 
-// |-----+-@----------| raw=57.9999999956, min=25, max=100 default=50
-PARAM_DEFINE PARAM_FUTILITY_MARGIN_ALPHA2 = 58;
+// 現在の値から、min～maxの範囲で、+step,-step,+0を試す。
+// interval = 2だと、-2*step,-step,+0,+step,2*stepの5つを試す。
 
-// |--------@--+------| raw=165.9999999952857, min=100, max=240 default=195
-PARAM_DEFINE PARAM_FUTILITY_MARGIN_BETA = 166;
+//
+// futility pruning
+//
 
-// |--------------@---| raw=146.99999999118182, min=50, max=160 default=145
-PARAM_DEFINE PARAM_FUTILITY_MARGIN_QUIET = 147;
+// 深さに比例したfutility pruning
+// depth手先で評価値が変動する幅が = depth * (PARAM_FUTILITY_MARGIN_ALPHA1 - improving*PARAM_FUTILITY_MARGIN_ALPHA2)
+// 元の値 = 175
+// [PARAM] min:100,max:240,step:2,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_FUTILITY_MARGIN_ALPHA1 = 172;
 
-// |------@-----------| raw=8.999999996, min=5, max=15 default=9
+// 元の値 = 50
+// [PARAM] min:25,max:100,step:2,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_FUTILITY_MARGIN_ALPHA2 = 50;
+
+
+// 
+
+// 元の値 = 200
+// [PARAM] min:100,max:240,step:2,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_FUTILITY_MARGIN_BETA = 195;
+
+
+// 静止探索でのfutility pruning
+// 元の値 = 128
+// [PARAM] min:50,max:160,step:5,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_FUTILITY_MARGIN_QUIET = 145;
+
+// futility pruningの適用depth。
+// この制限自体が要らない可能性がある。→　そうでもなかった。
+// 元の値 = 7
+// [PARAM] min:5,max:15,step:1,interval:1,time_rate:1,fixed
 PARAM_DEFINE PARAM_FUTILITY_RETURN_DEPTH = 9;
 
-// |--@----+----------| raw=6.999999998666667, min=5, max=20 default=12
-PARAM_DEFINE PARAM_FUTILITY_AT_PARENT_NODE_DEPTH = 7;
+// 親nodeでのfutilityの適用depth。
+// この枝刈り、depthの制限自体が要らないような気がする。→　そうでもなかった。
+// 元の値 = 7
+// [PARAM] min:5,max:20,step:1,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_FUTILITY_AT_PARENT_NODE_DEPTH = 12;
 
-// |----@--------+----| raw=151.99999999738694, min=100, max=300 default=256
-PARAM_DEFINE PARAM_FUTILITY_AT_PARENT_NODE_MARGIN1 = 152;
+// 親nodeでのfutility margin
+// 元の値 = 256
+// [PARAM] min:100,max:300,step:5,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_FUTILITY_AT_PARENT_NODE_MARGIN1 = 256;
 
-// |---@----------+---| raw=56.999999998099995, min=0, max=300 default=248
-PARAM_DEFINE PARAM_FUTILITY_AT_PARENT_NODE_MARGIN2 = 57;
+// staticEvalから減算するmargin
+// 元の値 = 200
+// [PARAM] min:0,max:300,step:25,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_FUTILITY_AT_PARENT_NODE_MARGIN2 = 248;
 
-// |-----------@------| raw=40.999999993, min=20, max=50 default=40
-PARAM_DEFINE PARAM_FUTILITY_AT_PARENT_NODE_GAMMA1 = 41;
+// depthが2乗されるので影響大きい
+// 元の値 = 35
+// [PARAM] min:20,max:50,step:1,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_FUTILITY_AT_PARENT_NODE_GAMMA1 = 40;
 
-// |--------@----+----| raw=40.99999999475, min=20, max=60 default=51
-PARAM_DEFINE PARAM_FUTILITY_AT_PARENT_NODE_GAMMA2 = 41;
+// depthが2乗されるので影響大きい
+// 元の値 = 35
+// [PARAM] min:20,max:60,step:3,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_FUTILITY_AT_PARENT_NODE_GAMMA2 = 51;
 
-// |--@--+------------| raw=663.99999999836, min=500, max=1500 default=818
-PARAM_DEFINE PARAM_NULL_MOVE_DYNAMIC_ALPHA = 664;
+//
+// null move dynamic pruning
+//
 
-// |-@---+------------| raw=54.999999998999996, min=50, max=100 default=67
-PARAM_DEFINE PARAM_NULL_MOVE_DYNAMIC_BETA = 55;
+// null move dynamic pruningのときの
+//  Reduction = (α + β * depth ) / 256 + ...みたいなαとβ
 
-// |-------+---@------| raw=44.999999992999996, min=10, max=60 default=31
-PARAM_DEFINE PARAM_NULL_MOVE_MARGIN = 45;
+// 元の値 = 823
+// [PARAM] min:500,max:1500,step:5,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_NULL_MOVE_DYNAMIC_ALPHA = 818;
 
-// |--@-----------+---| raw=5.999999998333333, min=4, max=16 default=14
-PARAM_DEFINE PARAM_NULL_MOVE_RETURN_DEPTH = 6;
+// 元の値 = 67
+// [PARAM] min:50,max:100,step:8,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_NULL_MOVE_DYNAMIC_BETA = 67;
 
-// |--@-+-------------| raw=3.9999999985714285, min=3, max=10 default=5
-PARAM_DEFINE PARAM_PROBCUT_DEPTH = 4;
+// 元の値 = 35
+// [PARAM] min:10,max:60,step:1,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_NULL_MOVE_MARGIN = 31;
 
-// |--------+-@-------| raw=218.9999999940909, min=100, max=300 default=202
-PARAM_DEFINE PARAM_PROBCUT_MARGIN1 = 219;
+// null moveでbeta値を上回ったときに、これ以下ならreturnするdepth。適用depth。
+// 元の値 = 12
+// [PARAM] min:4,max:16,step:1,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_NULL_MOVE_RETURN_DEPTH = 14;
 
-// |@+----------------| raw=21.999999999666667, min=20, max=80 default=24
-PARAM_DEFINE PARAM_PROBCUT_MARGIN2 = 22;
 
-// |-----+-@----------| raw=7.999999995555555, min=4, max=13 default=7
-PARAM_DEFINE PARAM_SINGULAR_EXTENSION_DEPTH = 8;
+//
+// ProbCut
+//
 
-// |----+------@------| raw=303.9999999935294, min=128, max=400 default=194
-PARAM_DEFINE PARAM_SINGULAR_MARGIN = 304;
+// probcutの前提depth
+// 元の値 = 5
+// [PARAM] min:3,max:10,step:1,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_PROBCUT_DEPTH = 5;
 
-// |--------+@--------| raw=20.999999994583334, min=8, max=32 default=20
-PARAM_DEFINE PARAM_SINGULAR_SEARCH_DEPTH_ALPHA = 21;
+// probcutのmargin
+//    式 = beta + PARAM_PROBCUT_MARGIN1 - improving * PARAM_PROBCUT_MARGIN2
+//   improvingの効果怪しいので抑え気味にしておく。
+// 元の値 = 216
+// [PARAM] min:100,max:300,step:3,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_PROBCUT_MARGIN1 = 194 + 16/2;
 
-// |-@---+------------| raw=9.999999999166667, min=8, max=32 default=16
-PARAM_DEFINE PARAM_PRUNING_BY_MOVE_COUNT_DEPTH = 10;
+// 元の値 = 48
+// [PARAM] min:20,max:80,step:2,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_PROBCUT_MARGIN2 = 48/2;
 
-// |--@+--------------| raw=6.999999998333333, min=2, max=32 default=9
-PARAM_DEFINE PARAM_PRUNING_BY_HISTORY_DEPTH = 7;
+//
+// singular extension
+//
 
-// |-----+-@----------| raw=4523.999999995796, min=2000, max=8000 default=4000
-PARAM_DEFINE PARAM_REDUCTION_BY_HISTORY = 4524;
+// singular extensionの前提depth。
+// これ変更すると他のパラメーターががらっと変わるので固定しておく。
+// 10秒設定だと6か8あたりに局所解があるようだ。
+// 元の値 = 8
+// [PARAM] min:4,max:13,step:1,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_SINGULAR_EXTENSION_DEPTH = 7;
 
-// |@---------+-------| raw=406.99999999976666, min=400, max=700 default=590
-PARAM_DEFINE PARAM_RAZORING_MARGIN2 = 407;
+// singular extensionのmarginを計算するときの係数
+// rBeta = std::max(ttValue - PARAM_SINGULAR_MARGIN * depth / (8 * ONE_PLY), -VALUE_MATE);
+// 元の値 = 256
+// [PARAM] min:128,max:400,step:4,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_SINGULAR_MARGIN = 194;
 
-// |------@----+------| raw=515.9999999961952, min=400, max=700 default=604
-PARAM_DEFINE PARAM_RAZORING_MARGIN3 = 516;
+// singular extensionで浅い探索をするときの深さに関する係数
+// このパラメーター、長い時間でないと調整できないし下手に調整すべきではない。
+// 元の値 = 16
+// [PARAM] min:8,max:32,step:2,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_SINGULAR_SEARCH_DEPTH_ALPHA = 20;
 
-// |----@-+-----------| raw=117.99999999717278, min=64, max=256 default=135
-PARAM_DEFINE PARAM_REDUCTION_ALPHA = 118;
 
-// |------+@----------| raw=261.999999995502, min=150, max=400 default=240
-PARAM_DEFINE PARAM_FUTILITY_MOVE_COUNT_ALPHA0 = 262;
+//
+// pruning by move count,history,etc..
+//
 
-// |---------@+-------| raw=459.99999999466667, min=300, max=600 default=492
-PARAM_DEFINE PARAM_FUTILITY_MOVE_COUNT_ALPHA1 = 460;
+// move countによる枝刈りをする深さ。適用depth。
+// 元の値 = 16
+// [PARAM] min:8,max:32,step:1,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_PRUNING_BY_MOVE_COUNT_DEPTH = 16;
 
-// |--+----@----------| raw=1141.9999999957172, min=500, max=2000 default=740
-PARAM_DEFINE PARAM_FUTILITY_MOVE_COUNT_BETA0 = 1142;
+// historyによる枝刈りをする深さ。適用depth。
+// これ、将棋ではそこそこ上げたほうが長い時間では良さげ。
+// 元の値 = 3
+// [PARAM] min:2,max:32,step:1,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_PRUNING_BY_HISTORY_DEPTH = 9;
 
-// |-----+@-----------| raw=1047.9999999963593, min=500, max=2000 default=1000
-PARAM_DEFINE PARAM_FUTILITY_MOVE_COUNT_BETA1 = 1048;
 
-// |-----+----@-------| raw=90.99999999385417, min=32, max=128 default=64
-PARAM_DEFINE PARAM_QUIET_SEARCH_COUNT = 91;
+// historyの値によってreductionするときの係数
+// これ、元のが (hist - 8000) / 20000みたいな意味ありげな値なので下手に変更しないほうが良さげ。
+// 元の値 = 4000
+// [PARAM] min:2000,max:8000,step:100,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_REDUCTION_BY_HISTORY = 4000;
 
-// |--+@--------------| raw=17.99999999785714, min=12, max=40 default=16
-PARAM_DEFINE PARAM_ASPIRATION_SEARCH_DELTA = 18;
 
-// |----+---@---------| raw=30.99999999475, min=10, max=50 default=20
-PARAM_DEFINE PARAM_EVAL_TEMPO = 31;
+//
+// razoring pruning
+// 
 
-// |----@----+--------| raw=8.9999999971875, min=0, max=32 default=17
-PARAM_DEFINE STAT_BONUS_DEPTH = 9;
-
-// |--------+-----@---| raw=53.999999991562504, min=0, max=64 default=32
-PARAM_DEFINE STAT_BONUS_COEFFICIENT2 = 54;
-
-// |--------+--@------| raw=85.99999999328125, min=0, max=128 default=64
-PARAM_DEFINE STAT_BONUS_COEFFICIENT1 = 86;
-
-// |--------+--@------| raw=86.99999999320312, min=0, max=128 default=64
-PARAM_DEFINE STAT_BONUS_COEFFICIENT0 = 87;
-
+// この値は、未使用。razoringはdepth < ONE_PLYでは行わないため。
+// 元の値 = 0
+// [PARAM] min:0,max:0,step:1,interval:2,time_rate:1,fixed
 PARAM_DEFINE PARAM_RAZORING_MARGIN1 = 0;
-PARAM_DEFINE PARAM_WEAK_MATE_PLY = 1;
+
+// 以下、変更しても計測できるほどの差ではないようなので元の値にしておく。
+// 元の値 = 590
+// [PARAM] min:400,max:700,step:10,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_RAZORING_MARGIN2 = 590;
+
+// 元の値 = 604
+// [PARAM] min:400,max:700,step:5,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_RAZORING_MARGIN3 = 604;
+
+
+//
+// LMR reduction table
+//
+
+// 元の値 = 131
+// [PARAM] min:64,max:256,step:2,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_REDUCTION_ALPHA = 135;
+
+
+//
+// futility move count table
+//
+
+// どうも、元の値ぐらいが最適値のようだ…。
+
+// 元の値 = 240
+// [PARAM] min:150,max:400,step:5,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_FUTILITY_MOVE_COUNT_ALPHA0 = 240;
+
+// 元の値 = 500
+// [PARAM] min:300,max:600,step:4,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_FUTILITY_MOVE_COUNT_ALPHA1 = 492;
+
+// 元の値 = 740
+// [PARAM] min:500,max:2000,step:10,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_FUTILITY_MOVE_COUNT_BETA0 = 740;
+
+// 元の値 = 1000
+// [PARAM] min:500,max:2000,step:5,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_FUTILITY_MOVE_COUNT_BETA1 = 1000;
+
+
+//
+// etc..
+// 
+
+// この個数までquietの指し手を登録してhistoryなどを増減させる。
+// 元の値 = 64
+// 将棋では駒打ちがあるから少し増やしたほうがいいかも。
+// →　そうでもなかった。固定しておく。
+// historyの計算でこの64に基づいた値を使っている箇所があるからのような気がする。よく考える。
+// [PARAM] min:32,max:128,step:2,interval:2,time_rate:1,fixed
+PARAM_DEFINE PARAM_QUIET_SEARCH_COUNT = 64;
+
+
+// 静止探索での1手詰め
+// 元の値 = 1
+// →　1スレ2秒で対技巧だと有りのほうが強かったので固定しておく。
+// [PARAM] min:0,max:1,step:1,interval:1,time_rate:1,fixed
 PARAM_DEFINE PARAM_QSEARCH_MATE1 = 1;
+
+// 通常探索での1手詰め
+// →　よくわからないが1スレ2秒で対技巧だと無しのほうが強かった。
+//     1スレ3秒にすると有りのほうが強かった。やはり有りのほうが良いのでは..
+// 元の値 = 1
+// [PARAM] min:0,max:1,step:1,interval:1,time_rate:1,fixed
 PARAM_DEFINE PARAM_SEARCH_MATE1 = 1;
 
-#endif // __PARAMETERS_GENERATED_HPP__
+// 1手詰めではなくN手詰めを用いる
+// 元の値 = 1
+// [PARAM] min:1,max:5,step:2,interval:1,time_rate:1,fixed
+PARAM_DEFINE PARAM_WEAK_MATE_PLY = 1;
+
+
+// aspiration searchの増加量
+// 元の値 = 15
+// [PARAM] min:12,max:40,step:1,interval:2,time_rate:1,fixed
+PARAM_DEFINE PARAM_ASPIRATION_SEARCH_DELTA = 16;
+
+
+// 評価関数での手番の価値
+// 元の値 = 20
+// [PARAM] min:10,max:50,step:5,interval:2,time_rate:1,fixed
+PARAM_DEFINE PARAM_EVAL_TEMPO = 20;
+
+// [PARAM] min:0,max:32,step:1,interval:1,time_rate:1,fixed
+PARAM_DEFINE STAT_BONUS_DEPTH = 17;
+
+// [PARAM] min:0,max:64,step:1,interval:1,time_rate:1,fixed
+PARAM_DEFINE STAT_BONUS_COEFFICIENT2 = 32;
+
+// [PARAM] min:0,max:128,step:1,interval:1,time_rate:1,fixed
+PARAM_DEFINE STAT_BONUS_COEFFICIENT1 = 64;
+
+// [PARAM] min:0,max:128,step:1,interval:1,time_rate:1,fixed
+PARAM_DEFINE STAT_BONUS_COEFFICIENT0 = 64;
+
+#endif
