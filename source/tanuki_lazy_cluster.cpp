c58a62d07fd03aa454c4dd9b7a3cb1116bb4577b
nodchip <nodchip@gmail.com>
2020-02-11 10:20:04 +0900

    LazyClusterで通信を非同期化した 終盤でLazyClusterがクラッシュするバグを修正した

--- source/tanuki_lazy_cluster.cpp
+++ source/tanuki_lazy_cluster.cpp
@@ -7,6 +7,7 @@
 
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
+#include <boost/smart_ptr.hpp>
 
 #include "misc.h"
 #include "thread.h"
@@ -25,11 +26,17 @@ namespace {
 	// 16バイト * 80 = 1280バイト
 	constexpr const int kMaxNumPacketsToSend = 80;
 
-	static std::shared_ptr<boost::asio::io_context> IO_CONTEXT;
+	static boost::asio::io_context IO_CONTEXT;
 	static std::shared_ptr<boost::asio::ip::udp::socket> UDP_SOCKET;
 	static std::vector<boost::asio::ip::udp::endpoint> ENDPOINTS;
-	static std::thread SERVER_THREAD;
+	static std::thread IO_CONTEXT_THREAD;
 	static std::atomic<bool> SERVER_RUNNING;
+	struct Initializer {
+		Initializer() {
+			SERVER_RUNNING = false;
+		}
+	} INITIALIZER;
+	static boost::array<Tanuki::LazyCluster::Packet, kMaxNumPacketsToSend> RECEIVE_BUFFER;
 
 	bool Serialize(Key key, Tanuki::LazyCluster::Packet& packet) {
 		bool found = false;
@@ -62,24 +69,28 @@ namespace {
 		entry->save(k, v, pv, b, d, m, ev);
 	}
 
-	void ServerProcedure() {
-		using boost::asio::ip::udp;
-
-		while (SERVER_RUNNING) {
-			boost::array<Tanuki::LazyCluster::Packet, kMaxNumPacketsToSend> recv_buffer = {};
-			boost::system::error_code error_code;
-			size_t bytes = UDP_SOCKET->receive(boost::asio::buffer(recv_buffer), 0, error_code);
-			if (error_code.failed()) {
-				sync_cout << "info string Falied to receive packets. error_code.message()=" << error_code.message() << sync_endl;
-				continue;
-			}
-
-			int num_packets = bytes / sizeof(Tanuki::LazyCluster::Packet);
+	void OnReceivePacket(const boost::system::error_code& error, std::size_t bytes_transferred) {
+		if (!error) {
+			int num_packets = bytes_transferred / sizeof(Tanuki::LazyCluster::Packet);
 			//sync_cout << "info string Lazy Cluster server: Received " << num_packets << " packets." << sync_endl;
 			for (int packet_index = 0; packet_index < num_packets; ++packet_index) {
-				Deserialize(recv_buffer[packet_index]);
+				Deserialize(RECEIVE_BUFFER[packet_index]);
 			}
 		}
+		else {
+			sync_cout << "info string Failed to receive packets. " << error << sync_endl;
+			return;
+		}
+
+		UDP_SOCKET->async_receive(boost::asio::buffer(RECEIVE_BUFFER), OnReceivePacket);
+	}
+
+	void OnSendPacket(std::vector<Tanuki::LazyCluster::Packet> /*packet*/,
+		const boost::system::error_code& error, std::size_t bytes_transferred) {
+		if (error) {
+			sync_cout << "info string Failed to send packets. " << error << sync_endl;
+		}
+		//sync_cout << "info string Lazy Cluster client: Sent " << bytes_transferred / sizeof(Tanuki::LazyCluster::Packet) << " packets." << sync_endl;
 	}
 }
 
@@ -94,6 +105,10 @@ void Tanuki::LazyCluster::InitializeLazyCluster(USI::OptionsMap& o) {
 void Tanuki::LazyCluster::Start() {
 	using boost::asio::ip::udp;
 
+	if (SERVER_RUNNING) {
+		return;
+	}
+
 	if (!static_cast<bool>(Options[kEnableLazyCluster])) {
 		return;
 	}
@@ -102,11 +117,9 @@ void Tanuki::LazyCluster::Start() {
 	// LazyClusterSendToオプションを解析し、アドレスとポートをENDPOINTSに格納する。
 	ENDPOINTS.clear();
 
-	IO_CONTEXT = std::make_shared< boost::asio::io_context>();
-
 	int port = static_cast<int>(Options[Tanuki::LazyCluster::kLazyClusterRecievePort]);
-	UDP_SOCKET = std::make_shared<udp::socket>(*IO_CONTEXT, udp::endpoint(udp::v4(), port));
-	udp::resolver resolver(*IO_CONTEXT);
+	UDP_SOCKET = std::make_shared<udp::socket>(IO_CONTEXT, udp::endpoint(udp::v4(), port));
+	udp::resolver resolver(IO_CONTEXT);
 
 	// 送信先のアドレスに分割する
 	std::string addresses = static_cast<std::string>(Options[kLazyClusterSendTo]);
@@ -129,8 +142,11 @@ void Tanuki::LazyCluster::Start() {
 		ENDPOINTS.push_back(receiver_endpoint);
 	}
 
+	UDP_SOCKET->async_receive(boost::asio::buffer(RECEIVE_BUFFER), OnReceivePacket);
 	SERVER_RUNNING = true;
-	SERVER_THREAD = std::thread(ServerProcedure);
+	IO_CONTEXT_THREAD = std::thread([]() {
+		IO_CONTEXT.run();
+		});
 }
 
 void Tanuki::LazyCluster::Stop() {
@@ -140,20 +156,13 @@ void Tanuki::LazyCluster::Stop() {
 		return;
 	}
 
+	if (!SERVER_RUNNING) {
+		return;
+	}
+
 	SERVER_RUNNING = false;
-
-	// サーバーに空の通信パケットを送り、ループを抜けさせる。
-	int port = Options[kLazyClusterRecievePort];
-	boost::asio::io_context io_context;
-	udp::resolver resolver(io_context);
-	udp::endpoint receiver_endpoint =
-		*resolver.resolve(udp::v4(), "localhost", std::to_string(port)).begin();
-	udp::socket socket(io_context);
-	socket.open(udp::v4());
-	boost::array<Packet, 0> send_buffer = {};
-	socket.send_to(boost::asio::buffer(send_buffer), receiver_endpoint);
-
-	SERVER_THREAD.join();
+	IO_CONTEXT.stop();
+	IO_CONTEXT_THREAD.join();
 }
 
 void Tanuki::LazyCluster::Send(Thread& thread) {
@@ -164,26 +173,28 @@ void Tanuki::LazyCluster::Send(Thread& thread) {
 	// 通信パケットにシリアライズする。
 	int multiPV = static_cast<int>(Options["MultiPV"]);
 	Position& position = thread.rootPos;
-	std::vector<Packet> packets;
+	auto packets = boost::make_shared<std::vector<Packet>>();
 
 	// 各PVについて処理する
+	multiPV = std::min<int>(multiPV, thread.rootMoves.size());
 	for (int pv_index = 0; pv_index < multiPV; ++pv_index) {
 		StateInfo state_info[MAX_PLY] = {};
 
 		Packet packet = {};
 		if (Serialize(position.key(), packet)) {
-			packets.push_back(packet);
+			packets->push_back(packet);
 		}
 
 		const auto& pv = thread.rootMoves[pv_index].pv;
-		for (auto move : pv) {
+		for (int moveIndex = 0; moveIndex < pv.size(); ++moveIndex) {
+			auto move = pv[moveIndex];
 			if (!is_ok(move)) {
 				break;
 			}
 
-			position.do_move(move, state_info[position.game_ply()]);
+			position.do_move(move, state_info[moveIndex]);
 			if (Serialize(position.key(), packet)) {
-				packets.push_back(packet);
+				packets->push_back(packet);
 			}
 		}
 		for (auto rit = pv.rbegin(); rit != pv.rend(); ++rit) {
@@ -192,18 +203,27 @@ void Tanuki::LazyCluster::Send(Thread& thread) {
 	}
 
 	// 通信パケットのサイズをUDPで1回で送りやすいサイズに減らす。
-	if (packets.size() > kMaxNumPacketsToSend) {
-		packets.resize(kMaxNumPacketsToSend);
+	if (packets->size() > kMaxNumPacketsToSend) {
+		packets->resize(kMaxNumPacketsToSend);
+	}
+
+	if (packets->empty()) {
+		return;
 	}
 
 	// 送信する。
 	for (const auto& receiver_endpoint : ENDPOINTS) {
-		boost::system::error_code error_code;
-		UDP_SOCKET->send_to(boost::asio::buffer(packets), receiver_endpoint, 0, error_code);
-		if (error_code.failed()) {
-			sync_cout << "info string Falied to send packets. error_code.message()=" << error_code.message() << sync_endl;
-			continue;
-		}
+		// 送信が完了するまでpacketsをメモリ上に保持し続けなければならない。
+		// ラムダ関数にキャプチャさせ、送信完了まで保持し続ける。
+		UDP_SOCKET->async_send_to(boost::asio::buffer(*packets), receiver_endpoint,
+			[packets](const boost::system::error_code& /*error*/, std::size_t bytes_transferred) {});
+
+		//boost::system::error_code error_code;
+		//UDP_SOCKET->send_to(boost::asio::buffer(packets), receiver_endpoint, 0, error_code);
+		//if (error_code.failed()) {
+		//	sync_cout << "info string Falied to send packets. error_code.message()=" << error_code.message() << sync_endl;
+		//	continue;
+		//}
 	}
 
 	//sync_cout << "info string Lazy Cluster client: Sent " << packets.size() << " packets." << sync_endl;
