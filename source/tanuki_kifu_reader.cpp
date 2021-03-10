ce835825cd94543094a812844a3618db1ee7d623
Hisayori Noda <nodchip@gmail.com>
2021-03-10 16:29:10 +0900

    FindFirstFile() を std::filesystem::directory_iterator で置き換えた

--- source/tanuki_kifu_reader.cpp
+++ source/tanuki_kifu_reader.cpp
@@ -1,13 +1,11 @@
-#include "tanuki_kifu_reader.h"
+ï»¿#include "tanuki_kifu_reader.h"
 #include "config.h"
 
 #ifdef EVAL_LEARN
 
+#include <filesystem>
 #include <numeric>
 #include <sstream>
-#define NOMINMAX
-#include <Windows.h>
-#undef NOMINMAX
 
 #include "misc.h"
 #include "usi.h"
@@ -36,17 +34,17 @@ bool Tanuki::KifuReader::Read(int num_records, std::vector<PackedSfenValue>& rec
 }
 
 bool Tanuki::KifuReader::Read(PackedSfenValue& record) {
-	// ƒtƒ@ƒCƒ‹ƒŠƒXƒg‚ğæ“¾‚µAƒtƒ@ƒCƒ‹‚ğŠJ‚¢‚½ó‘Ô‚É‚·‚é
+	// ãƒ•ã‚¡ã‚¤ãƒ«ãƒªã‚¹ãƒˆã‚’å–å¾—ã—ã€ãƒ•ã‚¡ã‚¤ãƒ«ã‚’é–‹ã„ãŸçŠ¶æ…‹ã«ã™ã‚‹
 	if (!EnsureOpen()) {
 		return false;
 	}
 
-	// ƒ‹[ƒvI—¹ğŒ‚ÍˆÈ‰º‚Ì’Ê‚è‚Æ‚·‚é
+	// ãƒ«ãƒ¼ãƒ—çµ‚äº†æ¡ä»¶ã¯ä»¥ä¸‹ã®é€šã‚Šã¨ã™ã‚‹
 	if (file_index_ == static_cast<int>(file_paths_.size()) && loop_ == num_loops_) {
 		return false;
 	}
 
-	// 1‹Ç–Ê“Ç‚İ‚Ş
+	// 1å±€é¢èª­ã¿è¾¼ã‚€
 	if (std::fread(&record, sizeof(record), 1, file_) == 1) {
 		return true;
 	}
@@ -68,8 +66,8 @@ bool Tanuki::KifuReader::Read(PackedSfenValue& record) {
 
 	file_ = std::fopen(file_paths_[file_index_].c_str(), "rb");
 	if (file_ == nullptr) {
-		// ƒtƒ@ƒCƒ‹‚ğŠJ‚­‚Ì‚É¸”s‚µ‚½‚ç
-		// “Ç‚İ‚İ‚ğI—¹‚·‚é
+		// ãƒ•ã‚¡ã‚¤ãƒ«ã‚’é–‹ãã®ã«å¤±æ•—ã—ãŸã‚‰
+		// èª­ã¿è¾¼ã¿ã‚’çµ‚äº†ã™ã‚‹
 		sync_cout << "into string Failed to open a kifu file: " << file_paths_[file_index_]
 			<< sync_endl;
 		return false;
@@ -100,30 +98,12 @@ bool Tanuki::KifuReader::EnsureOpen() {
 		return true;
 	}
 
-	// http://qiita.com/tkymx/items/f9190c16be84d4a48f8a
-	HANDLE find = nullptr;
-	WIN32_FIND_DATAA find_data = {};
-
-	std::string search_name = folder_name_ + "/*.*";
-	find = FindFirstFileA(search_name.c_str(), &find_data);
-
-	if (find == INVALID_HANDLE_VALUE) {
-		sync_cout << "Failed to find kifu files." << sync_endl;
-		sync_cout << "search_name=" << search_name << sync_endl;
-		return false;
-	}
-
-	do {
-		if (find_data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+	for (const auto& entry : std::filesystem::directory_iterator(folder_name_)) {
+		if (!entry.is_regular_file()) {
 			continue;
 		}
-
-		std::string file_path = folder_name_ + "/" + find_data.cFileName;
-		file_paths_.push_back(file_path);
-	} while (FindNextFileA(find, &find_data));
-
-	FindClose(find);
-	find = nullptr;
+		file_paths_.push_back(entry.path().string());
+	}
 
 	if (file_paths_.empty()) {
 		return false;
