51f280a54346912026fd8aa5ff6ffe356696cf8c
Hisayori Noda <nodchip@gmail.com>
2021-11-10 10:33:21 +0900

    Jenkins操作用スクリプトの整備

--- script/execute_jenkins_jobs.py
+++ script/execute_jenkins_jobs.py
@@ -15,129 +15,37 @@ def main():
                         help='User name ex) nodchip', required=True)
     args = parser.parse_args()
 
-    for index in range(0, 7):
+    for index, parameter in enumerate([-500,-400,-300,-200,-100,-2,100,200,300,400,500]):
         thread_id_offset = index * 16 % 128
         numa_node = thread_id_offset // 64
 
         # Parameterized Build - Jenkins - Jenkins Wiki https://wiki.jenkins.io/display/JENKINS/Parameterized+Build
-        # query = urllib.parse.urlencode({
-        #     'KifuDir': fr'D:\hnoda\shogi\training_data\tanuki-wcsc28.depth={depth}',
-        #     'ShuffledKifuDir': fr'D:\hnoda\shogi\training_data\tanuki-wcsc28.depth={depth}.shuffled',
-        #     'KifuDirForTest': fr'D:\hnoda\shogi\validation_data\tanuki-wcsc28.depth={depth}',
-        #     'ShuffledKifuDirForTest': fr'D:\hnoda\shogi\validation_data\tanuki-wcsc28.depth={depth}.shuffled',
-        # })
 
+        # winning_percentage_for_win = fr'{parameter:0.6f}'
         # query = urllib.parse.urlencode({
-        #     'EvalDir': fr'D:\hnoda\tnk-wcsc28-2018-05-05\eval',
-        #     'EvalSaveDir': fr'D:\hnoda\shogi\eval\tanuki-wcsc28.depth={depth}',
-        #     'targetdir': fr'D:\hnoda\shogi\training_data\tanuki-wcsc28.depth={depth}.shuffled',
-        #     'eta': fr'0.01',
-        #     'validation_set_file_name': fr'D:\hnoda\shogi\validation_data\tanuki-wcsc28.depth={depth}.shuffled\xaa',
+        #     'EvalDir': fr'D:\hnoda\tanuki-wcsc29-2019-05-06\eval',
+        #     'EvalSaveDir': fr'D:\hnoda\shogi\eval\tanuki-wcsc29-2019-05-06.winning_percentage_for_win={winning_percentage_for_win}',
+        #     'targetdir': fr'D:\hnoda\shogi\training_data\suisho-wcsoc2020.shuffled',
+        #     'eta': fr'1.0',
+        #     'validation_set_file_name': fr'D:\hnoda\shogi\validation_data\suisho-wcsoc2020.shuffled\xaa',
         #     'ThreadIdOffset': fr'{index * 16 % 128}',
+        #     'winning_percentage_for_win': fr'{winning_percentage_for_win}',
+        #     'numa_node': fr'{numa_node}',
+        #     'weight_by_progress': '1',
         # })
 
+        # winning_percentage_for_win = fr'{parameter:0.6f}'
         # query = urllib.parse.urlencode({
-        #     'eval1': fr'D:\hnoda\tanuki-denryu-tsec-1\eval',
+        #     'eval1': fr'D:\hnoda\shogi\eval\tanuki-wcsc29-2019-05-06.winning_percentage_for_win={winning_percentage_for_win}\final',
         #     'eval2': fr'D:\hnoda\tanuki-denryu-tsec-1\eval',
-        #     'slow_mover1': fr'{slow_mover}',
         # })
 
-        # query = urllib.parse.urlencode({
-        #     'eval1': fr'D:\hnoda\shogi\eval\tanuki-wcsc28.depth={depth}\final',
-        #     'eval2': fr'D:\hnoda\tnk-wcsc28-2018-05-05\eval',
-        # })
-
-        # l = f'{index/10.0:.1f}'
-        # query = urllib.parse.urlencode({
-        #     'EvalDir': fr'D:\hnoda\tnk-wcsc28-2018-05-05\eval',
-        #     'EvalSaveDir': fr'D:\hnoda\shogi\eval\tanuki-wcsc28.depth=9.lambda={l}',
-        #     'targetdir': fr'D:\hnoda\shogi\training_data\tanuki-wcsc28.depth=9.shuffled',
-        #     'eta': fr'0.01',
-        #     'validation_set_file_name': fr'D:\hnoda\shogi\validation_data\tanuki-wcsc28.depth=9.shuffled\xaa',
-        #     'ThreadIdOffset': fr'{index * 16 % 128}',
-        #     'lambda': l,
-        # })
-
-        # l = f'{index/10.0:.1f}'
-        # query = urllib.parse.urlencode({
-        #     'eval1': fr'D:\hnoda\shogi\eval\tanuki-wcsc28.depth=9.lambda={l}\final',
-        #     'eval2': fr'D:\hnoda\tnk-wcsc28-2018-05-05\eval',
-        # })
-
-        # p = index * 0.5 + 9.0
-        # eval_limit = int(2.0 ** p)
-        # query = urllib.parse.urlencode({
-        #     'EvalDir': fr'D:\hnoda\tnk-wcsc28-2018-05-05\eval',
-        #     'EvalSaveDir': fr'D:\hnoda\shogi\eval\tanuki-wcsc28.depth=9.lambda=0.4.eval_limit={eval_limit}',
-        #     'targetdir': fr'D:\hnoda\shogi\training_data\tanuki-wcsc28.depth=9.shuffled',
-        #     'eta': fr'0.01',
-        #     'validation_set_file_name': fr'D:\hnoda\shogi\validation_data\tanuki-wcsc28.depth=9.shuffled\xaa',
-        #     'ThreadIdOffset': fr'{128 - 16 - index * 16 % 128}',
-        #     'lambda': fr'0.4',
-        #     'eval_limit': fr'{eval_limit}'
-        # })
-
-        # p = index * 0.5 + 9.0
-        # eval_limit = int(2.0 ** p)
-        # query = urllib.parse.urlencode({
-        #     'eval1': fr'D:\hnoda\shogi\eval\tanuki-wcsc28.depth=9.lambda=0.4.eval_limit={eval_limit}\final',
-        #     'eval2': fr'D:\hnoda\tnk-wcsc28-2018-05-05\eval',
-        # })
-
-        # winning_percentage_for_win = fr'{1.0 - 2.0 ** (-(index + 2)):0.6f}'
-        # query = urllib.parse.urlencode({
-        #     'EvalDir': fr'D:\hnoda\tnk-wcsc28-2018-05-05\eval',
-        #     'EvalSaveDir': fr'D:\hnoda\shogi\eval\tanuki-wcsc28.winning_percentage_for_win={winning_percentage_for_win}',
-        #     'targetdir': fr'D:\hnoda\shogi\training_data\tanuki-wcsc28.depth=9.shuffled',
-        #     'eta': fr'0.01',
-        #     'validation_set_file_name': fr'D:\hnoda\shogi\validation_data\tanuki-wcsc28.depth=9.shuffled\xaa',
-        #     'ThreadIdOffset': fr'{index * 16 % 128}',
-        #     'winning_percentage_for_win': fr'{winning_percentage_for_win}'
-        # })
-
-        # winning_percentage_for_win = fr'{1.0 - 2.0 ** (-(index + 2)):0.6f}'
-        # if index == 10:
-        #     winning_percentage_for_win = fr'0.999999'
-        # query = urllib.parse.urlencode({
-        #     'eval1': fr'D:\hnoda\shogi\eval\tanuki-wcsc28.winning_percentage_for_win={winning_percentage_for_win}\final',
-        #     'eval2': fr'D:\hnoda\tnk-wcsc28-2018-05-05\eval',
-        # })
-
-        # l2_regularization_parameter = fr'{10.0 ** (-(index + 1)):0.8f}'
-        # query = urllib.parse.urlencode({
-        #     'EvalDir': fr'D:\hnoda\tnk-wcsc28-2018-05-05\eval',
-        #     'EvalSaveDir': fr'D:\hnoda\shogi\eval\tanuki-wcsc28.l2_regularization_parameter={l2_regularization_parameter}',
-        #     'targetdir': fr'D:\hnoda\shogi\training_data\tanuki-wcsc28.depth=9.shuffled',
-        #     'eta': fr'0.01',
-        #     'validation_set_file_name': fr'D:\hnoda\shogi\validation_data\tanuki-wcsc28.depth=9.shuffled\xaa',
-        #     'ThreadIdOffset': fr'{index * 16 % 128}',
-        #     'l2_regularization_parameter': fr'{l2_regularization_parameter}'
-        # })
-
-        # l2_regularization_parameter = fr'{10.0 ** (-(index + 1)):0.8f}'
-        # query = urllib.parse.urlencode({
-        #     'eval1': fr'D:\hnoda\shogi\eval\tanuki-wcsc28.l2_regularization_parameter={l2_regularization_parameter}\final',
-        #     'eval2': fr'D:\hnoda\tnk-wcsc28-2018-05-05\eval',
-        # })
-
-        nn_batch_size = int(10.0 ** ((index / 2.0) + 2.0))
-        eta = 10.0 ** ((index / 2.0) - 3.0)
+        draw_value1 = fr'{parameter}'
         query = urllib.parse.urlencode({
-            'EvalDir': fr'D:\hnoda\tnk-wcsc28-2018-05-05\eval',
-            'EvalSaveDir': fr'D:\hnoda\shogi\eval\tanuki-wcsc28.nn_batch_size={nn_batch_size}',
-            'targetdir': fr'D:\hnoda\shogi\training_data\tanuki-wcsc28.depth=9.shuffled',
-            'eta': fr'{eta}',
-            'validation_set_file_name': fr'D:\hnoda\shogi\validation_data\tanuki-wcsc28.depth=9.shuffled\xaa',
-            'ThreadIdOffset': fr'{thread_id_offset}',
-            'numa_node': fr'{numa_node}',
-            'nn_batch_size': fr'{nn_batch_size}'
+            'eval1': fr'D:\hnoda\tanuki-denryu-tsec-1\eval',
+            'eval2': fr'D:\hnoda\tanuki-denryu-tsec-1\eval',
+            'draw_value1': fr'{draw_value1}',
         })
-
-        # nn_batch_size = int(10.0 ** ((index / 2.0) + 2.0))
-        # query = urllib.parse.urlencode({
-        #     'eval1': fr'D:\hnoda\shogi\eval\tanuki-wcsc28.nn_batch_size={nn_batch_size}\final',
-        #     'eval2': fr'D:\hnoda\tnk-wcsc28-2018-05-05\eval',
-        # })
 
         url = f'http://{args.host_name}:8080/job/{args.projet_name}/buildWithParameters?{query}'
         print(url)
