448bc5274ed7154941272e1aaf50ea33000b6984
yaneruao <yaneurao@gmail.com>
2021-12-05 02:11:13 +0900

    - Revert and fix earlier windows NUMA patch 	- https://github.com/official-stockfish/Stockfish/commit/7218ec4df9fef1146a451b71f0ed3bfd8123c9f9
    	> Revert and fix earlier windows NUMA patch
    	> revert 9048ac0 due to core spread problem and fix new OS 
    compatibility with another method.
    
    	以前のWindows NUMAパッチを元に戻して修正
    	9048ac0のコア分散問題を元に戻し、新しいOSとの互換性を別の方法で修正しました。
    
    - Do more aggressive pruning for some node types
    	- 
    https://github.com/official-stockfish/Stockfish/commit/e4b7403f127a36a35bbace9f833ab43babd98a6c
    
    	Enable compilation on older Windows systems
    	Improve compatibility of the last NUMA patch when running under older 
    versions of Windows,
    	for instance Windows Server 2003. 
    
    	古いWindowsシステムでのコンパイルを可能にする
    	旧バージョンのWindows上で動作するNUMAパッチの互換性を向上させました。
    	最後のNUMAパッチの互換性を向上させます（Windows Server 2003など）。

--- source/misc.cpp
+++ source/misc.cpp
@@ -25,6 +25,8 @@ extern "C" {
 		PSYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX, PDWORD);
 	typedef bool(*fun2_t)(USHORT, PGROUP_AFFINITY);
 	typedef bool(*fun3_t)(HANDLE, CONST GROUP_AFFINITY*, PGROUP_AFFINITY);
+	typedef bool(*fun4_t)(USHORT, PGROUP_AFFINITY, USHORT, PUSHORT);
+	typedef WORD(*fun5_t)();
 }
 
 #endif
@@ -575,11 +577,11 @@ namespace WinProcGroup {
 #else
 
 
-	/// best_group() retrieves logical processor information using Windows specific
-	/// API and returns the best group id for the thread with index idx. Original
+	/// best_node() retrieves logical processor information using Windows specific
+	/// API and returns the best node id for the thread with index idx. Original
 	/// code from Texel by Peter Österlund.
 
-	int best_group(size_t idx) {
+	int best_node(size_t idx) {
 
 		// スレッド番号idx(0 ～ 論理コア数-1)に対して
 		// 適切なNUMA NODEとCPU番号を設定する。
@@ -603,7 +605,8 @@ namespace WinProcGroup {
 		if (!fun1)
 			return -1;
 
-		// First call to get returnLength. We expect it to fail due to null buffer
+		// First call to GetLogicalProcessorInformationEx() to get returnLength.
+		// We expect the call to fail due to null buffer.
 		if (fun1(RelationAll, nullptr, &returnLength))
 			return -1;
 
@@ -678,22 +681,35 @@ namespace WinProcGroup {
 		// Use only local variables to be thread-safe
 
 		// 使うべきプロセッサグループ番号が返ってくる。
-		int group = best_group(idx);
+		int node = best_node(idx);
 
-		if (group == -1)
+		if (node == -1)
 			return;
 
 		// Early exit if the needed API are not available at runtime
 		HMODULE k32 = GetModuleHandle(L"Kernel32.dll");
 		auto fun2 = (fun2_t)(void(*)())GetProcAddress(k32, "GetNumaNodeProcessorMaskEx");
 		auto fun3 = (fun3_t)(void(*)())GetProcAddress(k32, "SetThreadGroupAffinity");
+		auto fun4 = (fun4_t)(void(*)())GetProcAddress(k32, "GetNumaNodeProcessorMask2");
 
 		if (!fun2 || !fun3)
 			return;
 
-		GROUP_AFFINITY affinity;
-		if (fun2(group, &affinity))
-			fun3(GetCurrentThread(), &affinity, nullptr);
+		if (!fun4) {
+			GROUP_AFFINITY affinity;
+			if (fun2(node, &affinity))
+				fun3(GetCurrentThread(), &affinity, nullptr);
+		} else {
+			// If a numa node has more than one processor group, we assume they are
+			// sized equal and we spread threads evenly across the groups.
+			USHORT elements, returnedElements;
+			elements = GetMaximumProcessorGroupCount();
+			GROUP_AFFINITY *affinity = (GROUP_AFFINITY*)malloc(
+				elements * sizeof(GROUP_AFFINITY));
+			if (fun4(node, affinity, elements, &returnedElements))
+				fun3(GetCurrentThread(), &affinity[idx % returnedElements], nullptr);
+			free(affinity);
+		}
 	}
 
 #endif
