d1dca56a66913cfaac23607dedd38540c2e23056
nodchip <nodchip@gmail.com>
2024-03-27 10:12:57 +0900

    halfkp_1024x2-16-32 ネットワークパラメーターを追加した。

--- source/eval/nnue/nnue_architecture.h
+++ source/eval/nnue/nnue_architecture.h
@@ -37,6 +37,11 @@
 // halfkp_1024x2-8-32型
 #include "architectures/halfkp_1024x2-8-32.h"
 
+#elif defined(YANEURAOU_ENGINE_NNUE_HALFKP_1024X2_16_32)
+
+// halfkp_1024x2-16-32型
+#include "architectures/halfkp_1024x2-16-32.h"
+
 #elif defined(EVAL_NNUE_HALFKP_VM_256X2_32_32)
 
 // halfkp_vm_256x2-32-32型
